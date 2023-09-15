20f400b3b5405ee9a2707983127dc076b49f532e
MarcoMazzilli <mazzillimarco97@gmail.com>
2023-09-15 15:04:28 +0200

    [add]Function to set perimeter

--- src/org/lessons/java/geometria/Main.java
+++ src/org/lessons/java/geometria/Main.java
@@ -4,12 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Rettangolo item1 = new Rettangolo();
+		Rettangolo item1 = new Rettangolo(10,5);
+		item1.getProperties();
 		
-		System.out.println("Base :" + item1.base);
-		System.out.println("Altezza :" + item1.altezza);
-		
-		Rettangolo item2 = new Rettangolo(10,5);
-		item2.getProperties();
 	}
 }
--- src/org/lessons/java/geometria/Rettangolo.java
+++ src/org/lessons/java/geometria/Rettangolo.java
@@ -1,22 +1,48 @@
 package org.lessons.java.geometria;
 
 public class Rettangolo {
-
+	
+	//PROPERTIES
 	public int base;
 	public int altezza;
+	public int area;
+	public int perimetro;
+	//PROPERTIES
 	
+	//CONTRUCTOR
 	Rettangolo() {
 		this.base = 0;
 		this.altezza = 0;
+		this.area = 0;
+		this.perimetro = 0;
 	}
 	
 	Rettangolo(int base, int altezza){
+		this();
+		
 		this.base = base;
 		this.altezza = altezza;
+		
+		setArea();
+		setPerimetro();
 	}
+	//CONTRUCTOR
 	
+	//GETTER
 	public void getProperties(){
 		System.out.println("Base: " + this.base);
 		System.out.println("Altezza: " + this.altezza);
+		System.out.println("area: " + this.area);
+		System.out.println("perimetro: " + this.perimetro);
+	}
+	
+	//SETTER
+	public void setArea(){
+		this.area = base * altezza;
+	}
+	
+	
+	public void setPerimetro(){
+		this.perimetro = (base*2) + (altezza*2);
 	}
 }
