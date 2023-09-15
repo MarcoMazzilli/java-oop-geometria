3c5af9475f8dadc2bd1723412d8f511d5b995ac2
MarcoMazzilli <mazzillimarco97@gmail.com>
2023-09-15 15:42:34 +0200

    [add]Loop to create rectangle

--- src/org/lessons/java/geometria/Main.java
+++ src/org/lessons/java/geometria/Main.java
@@ -1,11 +1,30 @@
 package org.lessons.java.geometria;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
-		Rettangolo item1 = new Rettangolo(10,5);
-		item1.getProperties();
+		Scanner sc = new Scanner(System.in);
+		
+		Rettangolo[] items = new Rettangolo[3];
+		
+		for (int i = 0; i < items.length; i++) {
+				
+			System.out.println("Inserisci la base: ");
+			int base = sc.nextInt();
+			System.out.println("Inserisci l'altezza: ");
+			int altezza= sc.nextInt();
+			
+			
+			items[i] = new Rettangolo(base,altezza);
+//			System.out.println("Proprieta' oggetto -> " + (i+1));
+			items[i].getProperties();
+		}
+		
+		sc.close();
+		
 		
 	}
 }
